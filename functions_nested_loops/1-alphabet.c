490306821abd8f7729b40c8cd36a19bf7994714b
EXTRADEEN <dinumira@gmail.com>
2022-10-05 10:01:01 -0700

    1.I sometimes suffer from insomnia. And when I cantn fall asleep, I play what I call The alphabet game

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - displays the alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char alpha;{
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		_putchar(alpha);
+}
+_putchar('\n');
+}
