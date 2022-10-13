e1df3f98e3db6ea0f52fe2432ace41863dc2ccb2
EXTRADEEN <dinumira@gmail.com>
2022-10-13 10:27:37 -0700

    6.Expected the best. Prepare for the wors. Capitalize on what comes

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - a function that capitalizes all words of a string
+ * @n: input string
+ * Return: caps on first letter of a separator
+ */
+char *cap_string(char *n)
+{
+	int i, s;
+	int cap = 31;
+	int sep[] = {',', ';', '.', '?', '"',
+		     '(', ')', '{', '}', ' ', '\n', '\t'};
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] -cap;
+		}
+
+		cap = 0;
+
+		for (s = 0; s <= 12; s++)
+		{
+			if (n[i] == sep[s])
+			{
+				s = 12;
+				cap = 32;
+			}
+		}
+	}
+	return (n);
+}
--- pointers_arrays_strings/README.md
+++ pointers_arrays_strings/README.md
@@ -14,3 +14,4 @@
 3-strcmp - Is a function that compares two strings.
 4-rev_array - Is a function that reverse the content of an array of integers.
 5-string_toupper - Is a function that changes all lowercase letters of a string to uppercase.
+6-cap_string - Is a function that capitalizes all word of a string.
