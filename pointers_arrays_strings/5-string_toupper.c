d84321d2519d985c77653a60093bf2bb99d0b73c
EXTRADEEN <dinumira@gmail.com>
2022-10-13 06:25:27 -0700

    5.Always look up

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * strig_trouper - s function that changes all lowercase letters
+ * of a string to uppercase
+ * @s: an input string
+ * Return: capitialized string
+ */
+char *string_toupper(char *s)
+{
+	int u;
+
+	for (u = 0; s[u] != '\0'; u++)
+	{
+		if (s[u] >= 'a' && s[u] <= 'z')
+			s[u] = s[u] - 32;
+	}
+	return (s);
+}
--- pointers_arrays_strings/README.md
+++ pointers_arrays_strings/README.md
@@ -13,3 +13,4 @@
 2-strncpy - Is a function that copies a string.
 3-strcmp - Is a function that compares two strings.
 4-rev_array - Is a function that reverse the content of an array of integers.
+5-string_toupper - Is a function that changes all lowercase letters of a string to uppercase.
