a00178e03767e6c92c02a2fa7049e7c4ebe99440
EXTRADEEN <dinumira@gmail.com>
2022-10-13 06:27:43 -0700

    5.Always look up

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strig_troupper - s function that changes all lowercase letters
+ * string_troupper - s function that changes all lowercase letters
  * of a string to uppercase
  * @s: an input string
  * Return: capitialized string
