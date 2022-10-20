925ef1d9ccb9dc882956bc1d4d3e9bfac3613eb7
EXTRADEEN <dinumira@gmail.com>
2022-10-20 03:53:04 -0700

    0.It ain"t what they call yoy, it"s what you answer to.

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name, followed by a new line
+ * @argc: argument count
+ * @argv: argument value, a string that come after caliing function
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+
+	printf("%s\n", argv[argc]);
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+0-whatsmyname - Is a program that prints its name, followed by a new line.
