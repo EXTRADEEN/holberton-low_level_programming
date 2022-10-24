6f862c82a8b3a3c43e5c43f695e9f7320e8d896f
EXTRADEEN <dinumira@gmail.com>
2022-10-24 13:17:01 -0700

    2.He who is not courageous enough to take risks will accomplish nothing in life.

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,37 +9,44 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int end1, end2, i = 0;
-	char *array;
+	char *new_str, *starts1, *starts2;
+	int i = 0, lens1 = 0, lens2 = 0;
 
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
-
-	for (end1 = 0; end1 <= *s1; end1++)
-	{
-	}
-
-	for (end2 = 0; end2 <= *s2; end2++)
-	{
-	}
-
-	array = malloc(sizeof(char) * (end1 + end2 + 1));
-
-	if (array == NULL)
-		return (NULL);
-
+	starts1 = s1;
+	starts2 = s2;
+	if (s1 == NULL)
+		s1 = "";
 	while (*s1)
 	{
-		array[i] = *s1;
-		i++;
+		lens1++;
 		s1++;
 	}
-
+	s1 = starts1;
+	if (s2 == NULL)
+		s2 = "";
 	while (*s2)
 	{
-		array[i] = *s2;
-		i++;
+		lens2++;
 		s2++;
 	}
-	return (array);
+	s2 = starts2;
+	new_str = malloc(sizeof(char) * (lens1 + lens2 + 1));
+	starts1 = new_str;
+	if (new_str == NULL)
+		return (NULL);
+	for (; i < (lens1 + lens2); i++)
+	{
+		if (i < lens1)
+		{
+			new_str[i] = *s1;
+			s1++;
+		}
+		else
+		{
+			new_str[i] = *s2;
+			s2++;
+		}
+	}
+	new_str[i] = '\0';
+	return (starts1);
 }
