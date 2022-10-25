3caca3a1fa54054488eaa24b659186cfdff316a7
EXTRADEEN <dinumira@gmail.com>
2022-10-25 01:59:50 -0700

    3.If you even dream of beating me you"d bettr wake up and apologize.

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -9,23 +9,36 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int **array, i = 0, j = 0;
+	int **arr;
+	int i, j;
 
-	if (width <= 0 || height <= 0)
-		return (NULL);
-
-	array = (int **)malloc(sizeof(int *) * height);
-	if (array == NULL)
-		return (NULL);
-	for (; i < height; i++)
+	if (height <= 0 || width <= 0)
 	{
-		array[i] = (int *)malloc(sizeof(int) * width);
-		if (array[i] == NULL)
-			return (NULL);
+		return (NULL);
+	}
+
+	arr = malloc(sizeof(int *) * height);
+
+	if (arr == NULL)
+	{
+		return (NULL);
 	}
 
 	for (i = 0; i < height; i++)
-		for (; j < width; j++)
-			array[i][j] = 0;
-	return (array);
+	{
+		arr[i] = malloc(sizeof(int) * width);
+		if (arr[i] == NULL)
+		{
+			for (i = i - 1; i >= 0; i--)
+			{
+				free(arr[i]);
+			}
+			free(arr);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; j < width; j++)
+		arr[i][j] = 0;
+	return (arr);
 }
