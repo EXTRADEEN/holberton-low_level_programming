1944ce6225409cffe85a2854c28933ff3ba6b30e
EXTRADEEN <dinumira@gmail.com>
2022-10-31 04:33:13 -0700

    0.Puppy

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+0. dog - Define a new type "struc dog"
---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
