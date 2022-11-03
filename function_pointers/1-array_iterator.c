66f1f80a4ed4435b2425c3cbf7839099a7b805d5
EXTRADEEN <dinumira@gmail.com>
2022-11-03 04:52:32 -0700

    task nr.2

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * array_iterator - executes function
+ * @array: pointer to array
+ * @size: structur size_t type
+ * @action: pointer to function
+ *
+ * Return - void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t n;
+
+	if (array == 0 || size == 0 || action == 0)
+		return;
+
+	for (n = 0; n < size; n++)
+	action(array[n]);
+}
