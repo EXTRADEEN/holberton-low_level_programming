eb89bb885d830a3d8d37c1faa6764f6861b6b873
EXTRADEEN <dinumira@gmail.com>
2022-11-04 03:57:10 -0700

    Task nr.0

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include <stdarg.h>
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
+#include <stdio.h>
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
