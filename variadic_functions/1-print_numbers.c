b17722424190f32f9f9a6a31651644c43ca29eff
EXTRADEEN <dinumira@gmail.com>
2022-11-04 04:46:24 -0700

    Task nr.2

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - a function that prints numbers
+ * followed by a new line
+ * @n: numbers of arguments
+ * @separator: separator
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i, j;
+
+	va_list ap;
+
+	va_start(ap, n);
+
+	j = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		j = va_arg(ap, unsigned int);
+		printf("%d", j);
+		if ( i == n -1)
+			continue;
+	if (separator != NULL)
+		printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
--- variadic_functions/README.md
+++ variadic_functions/README.md
@@ -1,1 +1,2 @@
 0-sum_them_all - Is a function that returns the sum of all its parameters.
+1-print_numbers - Is a function that prints nubers
