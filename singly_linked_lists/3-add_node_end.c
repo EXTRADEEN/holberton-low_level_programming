518266a306a18ac161b85b0c80294cd7cf40e0f9
EXTRADEEN <dinumira@gmail.com>
2022-11-16 06:16:49 -0800

    Task 3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -9,25 +9,37 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	char *s;
-	int i = 0;
-	list_t *new;
+	char *cp_str;
+	int len;
+	list_t *new, *end;
 
-	s = strdup(str);
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
+		return (NULL);
+
+	cp_str = strdup(str);
+	if (cp_str == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	new->str = s;
-	new->len = i;
-	new->next = *head;
-	*head = new;
 
-	return (new);
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = cp_str;
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+
+	else
+	{
+		end = *head;
+		while (end->next != NULL)
+			end = end->next;
+		end->next = new;
+	}
+	return (*head);
 }
