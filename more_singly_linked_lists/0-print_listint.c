5803c3af41aa99df4ecc84171dd8e0d40182a921
EXTRADEEN <dinumira@gmail.com>
2022-11-17 04:23:53 -0800

    Task 0 - print elements of a linked list

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - is a function that prints elements of a list
+ * @h: pointer to the structure
+ * Return: the numbers of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
