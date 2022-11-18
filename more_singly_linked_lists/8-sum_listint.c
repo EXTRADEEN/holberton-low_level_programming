bafec9fe92b8f9d7d98c452bc63cd0a49c48c12a
EXTRADEEN <dinumira@gmail.com>
2022-11-18 03:18:42 -0800

    betty style fixed

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sim_listint - function that returns the sum of all data (n),
+ * sum_listint - function that returns the sum of all data (n),
  * of a listint_t linked list
  * @head: pointer to the linked list
  * Return: sum of data,
