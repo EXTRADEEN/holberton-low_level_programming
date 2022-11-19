97e08fe7575319b9c962dc5f51b5e1832cbff2f9
EXTRADEEN <dinumira@gmail.com>
2022-11-19 12:14:40 -0800

    main.h - fixed a WARNING, 0-binary_to_unit - fixed description

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_int - function that converts a binary number to an unsigned int
+ * binary_to_uint - function that converts a binary number to an unsigned int
  * @b: pointer to a string
  * Return: the converted number, or 0 if,
  * there is one or more chars in the string b that is not 0 or 1
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,8 +1,8 @@
-#ifndef _MAIN_H
+#ifndef _MAIN_H_
 #define _MAIN_H_
 #include <stdlib.h>
 
 void _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 
-#endif /* _MAIN_H */
+#endif /* _MAIN_H_ */
