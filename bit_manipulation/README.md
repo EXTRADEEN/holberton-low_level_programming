131ac3370df4618e25daa9261b9e5b53f4c01e10
EXTRADEEN <dinumira@gmail.com>
2022-11-19 12:10:48 -0800

    Task 1

---
+++ bit_manipulation/README.md
@@ -0,0 +1,1 @@
+0-binary_to_init - A function that converts a binary number to an unsigned int
---
+++ bit_manipulation/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H
+#define _MAIN_H_
+#include <stdlib.h>
+
+void _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif /* _MAIN_H */
