2fcf9398fb75c6052830e117dfb54c3e9b90536b
EXTRADEEN <dinumira@gmail.com>
2022-11-22 01:58:51 -0800

    Task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,22 +10,25 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, r;
+	int fd, i;
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC | 0600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
 	if (fd == -1)
 		return (-1);
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
-	for (r = 0; text_content[r] != '\0'; r++)
-	{
-	}
-	write(fd, text_content, r);
-	}
+		for (i = 0; text_content[i] != '\0'; i++)
+		{
 
+		}
+		write(fd, text_content, i);
+	}
 	close(fd);
+
 	return (1);
 }
