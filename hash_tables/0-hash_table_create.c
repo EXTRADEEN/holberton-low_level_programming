140ad61bdb8fd08baeaea333e347eeb359b35321
EXTRADEEN <dinumira@gmail.com>
2022-11-27 10:57:54 -0800

    betty style fix

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,34 +1,26 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - function that creates a hash table
- * @size: size of the array
- * Return: a pointer to the newly created hash table
- * if something went wrong, your function should return NULL
+ *hash_table_create - function that creates a hash table
+ *@size: size of the array
+ *
+ *Return: pointer to the newly created hash table or NULL
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-	hash_node_t **new_array;
 	unsigned long int i;
 
-	if (size == 0)
-		return (NULL);
-
-	new_table = malloc(sizeof(hash_table_t)); /*create hash table*/
+	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 
-	new_array = malloc(sizeof(hash_node_t *) * size); /*create an array*/
-	if (new_array == NULL)
-		return (NULL);
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	for (i = 0; i < size; i++)
-		new_array[i] = NULL;
-
-	new_table->array = new_array;
-	new_table->size = size;
+		new_table->array[i] = NULL;
 
 	return (new_table);
 }
