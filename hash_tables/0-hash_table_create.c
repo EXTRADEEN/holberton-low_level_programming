e9edcb51e902acf1fc40118903ccfd12e2dfb8e6
EXTRADEEN <dinumira@gmail.com>
2022-11-27 10:39:59 -0800

    betyy style fix

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,7 +2,7 @@
 
 /**
  * hash_table_create - function that creates a hash table
- * #size: size of the array
+ * @size: size of the array
  * Return: a pointer to the newly created hash table
  * if something went wrong, your function should return NULL
  */
